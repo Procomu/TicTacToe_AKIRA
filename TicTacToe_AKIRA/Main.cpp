1445ae6076055b4c8e90e4047f698b205cbb87f2
Pro Comu <procomu.git@gmail.com>
2014-10-07 00:02:01 +0900

    add all function and value

--- TicTacToe_AKIRA/Main.cpp
+++ TicTacToe_AKIRA/Main.cpp
@@ -1,6 +1,44 @@
 #include <stdio.h>
 #define _CRT_SECURE_NO_WARNING
 
+int board[3][3];		// -1, 0, 1
+int stone;				// -1, 1
+int player;				// 0, 1
+char row;				// a, b, c
+int temp_row;			// 0, 1, 2
+int column;				// 0, 1, 2
+int judge;				// 0, 1
+
+void changeStone() {
+}
+
+void changePlayer() {
+}
+
+void dispWinner() {
+}
+
+void checkWin() {
+}
+
+void putableStone() {
+}
+
+void input() {
+}
+
+void showBoard() {
+}
+
+void initPlayer() {
+}
+
+void initStone() {
+}
+
+void initBoard() {
+}
+
 int main() {
 	return 0;
 }

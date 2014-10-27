929f27bf796f377d59aca19b81c125825ce6d8f8
Pro Comu <procomu.git@gmail.com>
2014-10-27 19:25:27 +0900

    add some comment. ver 2.

--- TicTacToe_AKIRA/Main.cpp
+++ TicTacToe_AKIRA/Main.cpp
@@ -3,7 +3,7 @@
 #include <math.h>
 #define _DEBUG
 
-//	define user value.
+// defineを使って変数のように数字を定義します。
 #define BOARD_SIZE 10
 #define EMPTY_CELL 0
 #define BLACK_STONE -1
@@ -11,43 +11,45 @@
 #define OUT_OF_CELL 9
 
 int player;	// 0, 1
-int stone;	// -1, 1
-int board[BOARD_SIZE][BOARD_SIZE];	// -1, 0, 1
+int stone;	// Xは-1, Oは1
+int board[BOARD_SIZE][BOARD_SIZE];	// 空かXかO
 char row;	// a, b, c ...
 int temp_row;	// 0, 1, 2 ...
 int column;	// 0, 1, 2 ...
 int putable;	// 0, 1
-int drow;	// 0, 1
-int judge;	// 0, 1
+int drow;	// 0だと続行,1だとドローゲーム
+int judge;	// 0だと勝者なし, 1だと勝者あり
 
 /*
- * initialize player value
+ * 変数playerを初期化する。
  */
 void initPlayer() {
 	 player = 0;
 }
 
 /*
- * initialize stone value
+ * 変数stoneを初期化する。
  */
 void initStone() {
 	 stone = BLACK_STONE;
 }
 
 /*
- * initialize board value
+ * 配列boardを初期化する。
  */
 void initBoard() {
 	for(int i=0; i<BOARD_SIZE; i++) {
 		for(int j=0; j<BOARD_SIZE; j++) {
 			switch(i) {
-				// 0 and BOARD_SIZE - 1 is Out of Board.
+				// 変数iが'0'もしくは'BOARD_SIZE-1'のときはボードの端っこなので、番兵を配置。
 				case 0:
 				case BOARD_SIZE - 1:
 					board[i][j] = OUT_OF_CELL;
 					break;
 				default:
+					// 空白を配置。
 					board[i][j] = EMPTY_CELL;
+					// 変数jが'0'もしくは'BOARD_SIZE-1'のときはボードの端っこなので、空白から番兵に置き換え。
 					if(0 == j || j == BOARD_SIZE - 1) {
 						board[i][j] = OUT_OF_CELL;
 					}
@@ -58,11 +60,13 @@ void initBoard() {
 }
 
 /*
- * show board
+ * 盤面を表示
  */
 void showBoard() {
 	printf("\n");
+// defineでDEBUGを宣言してやると、以下の#ifdef内が実行される。
 #ifdef DEBUG
+// 盤面をきれいに整形せず、生のデータで表示する。
 	for(int i=0; i<BOARD_SIZE; i++) {
 		for(int j=0; j<BOARD_SIZE; j++) {
 			printf("%d", board[i][j]);
@@ -71,6 +75,7 @@ void showBoard() {
 	}
 	printf("\n");
 # else
+// 盤面をきれいに整形して表示する。
 	printf("   ");
 	for(int i=1; i<BOARD_SIZE - 1; i++) {
 		printf("%d ", i);
@@ -87,14 +92,17 @@ void showBoard() {
 		for(int j=1; j<BOARD_SIZE - 1; j++) {
 			printf("|");
 			switch(board[i][j]) {
+			// 空だったら空白を表示。
 			case EMPTY_CELL:
 				printf(" ");
 				break;
+			// -1だったらXを表示。
 			case BLACK_STONE:
-				printf("O");
-				break;
-			case WHITE_STONE:
 				printf("X");
+				break;
+			// 1だったらOを表示。
+			case WHITE_STONE:
+				printf("O");
 				break;
 			default:
 				break;
@@ -110,7 +118,11 @@ void showBoard() {
 #endif
 }
 
+/*
+ * 指定したセルに石を置けるかを判断する。
+ */
 void putablestone() {
+	// セルに石が置けたら変数putableを0に、置けなければ1にする。
 	if(EMPTY_CELL == board[temp_row][column]){
 		putable = 0;
 	}else{
@@ -119,26 +131,33 @@ void putablestone() {
 	}
 }
 
+/*
+ * row(行)の入力を処理する。
+ */
 void inputRow() {
 	printf("Please input row (a, b, c ...) : ");
 	fflush(stdin);
 	scanf("%c", &row);
+	// 'a'という文字を数字(文字コード)に直す。
 	int c = 'a';
+	// 入力された文字-('a'の文字コード+1)の結果をセルの番地とする。
 	temp_row = (int)row - c + 1;
-	if(temp_row<=0) {
-		printf("Please retry\n");
-		inputRow();
-	} else if(BOARD_SIZE-1<=temp_row) {
+	// 変換後の値が0より小さい場合、ボードのサイズより大きい場合は再度入力へ。
+	if(temp_row < 1　|| (BOARD_SIZE - 2) < temp_row) {
 		printf("Please retry\n");
 		inputRow();
 	}
 	return;
 }
 
+/*
+ * column(列)の入力を処理する。
+ */
 void inputColumn() {
 	printf("Please input column (1, 2, 3 ...) : ");
 	fflush(stdin);
 	scanf("%d", &column);
+	// 入力された値が1より小さい場合、もしくはボードのサイズより大きい場合は再度入力へ。
 	if(column < 1 || (BOARD_SIZE - 2) < column) {
 		printf("Please retry\n");
 		inputColumn();
@@ -146,6 +165,9 @@ void inputColumn() {
 	return;
 }
 
+/*
+ * 勝者が居るかどうかを判断する。
+ */
 void checkWin(int flag, int count, int check_row, int check_column, int index_dir) {
 	if(index_dir < 4) {
 		int dir_r[4] ={-1, -1, 0, 1};
@@ -156,28 +178,34 @@ void checkWin(int flag, int count, int check_row, int check_column, int index_di
 		printf("\n");
 #endif
 		if(OUT_OF_CELL == board[check_row + (dir_r[index_dir] * flag)][check_column + (dir_c[index_dir] * flag)]) {
+		// ボードの外(番兵)だったら、フラグを反転させ、逆向きを走査する。
 			if(1 == flag) {
 				flag = -1;
 				checkWin(flag, count, temp_row, column, index_dir);
 			} else if(-1 == flag) {
+				// BOARD_SIZE-3とカウントが一致した場合、勝者が居るという判断をする。
 				if(BOARD_SIZE - 3 == count) {
 					judge = 1;
 				} else {
+				// BOARD_SIZE-3とカウントが一致しない場合、勝者が居ないという判断をする。
 					flag = 1;
 					count = 0;
 					checkWin(flag, count, temp_row, column, ++index_dir);
 				}
 			}
 		} else if(EMPTY_CELL == board[check_row + (dir_r[index_dir] * flag)][check_column + (dir_c[index_dir] * flag)]) {
+		// 空のセルだったら、フラグを初期化し、次の方向を走査する。
 			drow = 1;
 			flag = 1;
 			count = 0;
 			checkWin(flag, count, temp_row, column, ++index_dir);
 		} else if(stone != board[check_row + (dir_r[index_dir] * flag)][check_column + (dir_c[index_dir] * flag)]) {
+		// 相手の石だったら、フラグを初期化し、次の方向を走査する。
 			flag = 1;
 			count = 0;
 			checkWin(flag, count, temp_row, column, ++index_dir);
 		} else if(stone == board[check_row + (dir_r[index_dir] * flag)][check_column + (dir_c[index_dir] * flag)]) {
+		// 自分の石だったら、同じ方向の走査を続行する。
 			int next_row = check_row + (dir_r[index_dir] * flag);
 			int next_column = check_column + (dir_c[index_dir] * flag);
 			checkWin(flag, ++count, next_row, next_column, index_dir);
@@ -186,18 +214,42 @@ void checkWin(int flag, int count, int check_row, int check_column, int index_di
 	return;
 }
 
+/*
+ * 勝者を表示する。
+ */
 void dispWinner() {
 	printf("The winner is Player%d!!\n", ++player);
 }
 
+/*
+ * 石を変える。
+ */
 void changeStone() {
+	// XならOへ、OならXへ変える。
 	stone *= -1;
 }
 
+/*
+ * プレイヤーを変える。
+ */
 void changePlayer() {
+	/*
+	 * XORを使っています。
+	 * XORとはビット演算です。
+	 * フラグが一緒の場合は0に、違う場合は1にします。
+	 * 
+	 *  0 ^ 0 = 0
+	 *  0 ^ 1 = 1
+	 *  1 ^ 0 = 1
+	 *  1 ^ 1 = 0
+	 */
 	player ^= 1;
 }
  		
+/*
+ * main関数です。
+ * プログラムが動き出したとき、一番初めに呼ばれる関数です。
+ */
 int main(int argc, char **argv) {
 	drow = 1;
 	printf("Game Start!!\n");
